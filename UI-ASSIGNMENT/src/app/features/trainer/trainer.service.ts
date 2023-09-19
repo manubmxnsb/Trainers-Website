b3891d069e63a469da08fafc8e2a9ac9e5c0680c
Manuel Marginean <manuel.marginean@red-to-blue.com>
2023-09-19 18:35:06 +0300

    Final Version 2.0

--- UI-ASSIGNMENT/src/app/features/trainer/trainer.service.ts
+++ UI-ASSIGNMENT/src/app/features/trainer/trainer.service.ts
@@ -13,15 +13,4 @@ export class TrainerService {
     console.log(this.http.get<TrainerInterface[]>("http://localhost:5130/api/trainers/all".toString()));
     return this.http.get<TrainerInterface[]>("http://localhost:5130/api/trainers/all");
   }
-/*   getTrainers(): Observable<TrainerInterface[]> {
-    const trainers = [
-      {name:'Alexandru Dumitrescu',sex:'male',age:25,yearsOfExperience:5,isAvailable:true,city:'Cluj'},
-      {name:'Anuța Pop',sex:'female',age:26,yearsOfExperience:3,isAvailable:true,city:'Cluj'},
-      {name:'Cătălin Pârvu',sex:'male',age:28,yearsOfExperience:1,isAvailable:true,city:'Cluj'},
-    ];
-    return of(trainers).pipe(delay(1000));
-  } */
-  
-  getAllTrainers(trainers$: Observable<TrainerInterface[]>): Observable<TrainerInterface[]> {
-    return trainers$;}
 }
